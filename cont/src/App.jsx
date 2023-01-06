4391c4a9e37b9c42950fd0308d0f9d7b7ac96b36
ray396 <rayssadantas31@gmail.com>
2023-01-06 16:38:18 -0300

    criação de projeto

--- cont/src/App.jsx
+++ cont/src/App.jsx
@@ -3,19 +3,22 @@ import Counter from './components/Counter';
 
 import niver from './assets/niver1.png';
 
+import useCountdown from './hooks/useCountdown';
+
 import './App.css';
 
 function App() {
+  const [day, hour, minute, second] = useCountdown("Jun 26, 2023 00:00:00");
 
   return (
     <div className="App" style={{backgroundImage: `url(${niver})`}}>
       <div className="container">
-        <Title title="countdown regressiva para 2023"/>
+        <Title title="countdown regressiva para meu aniversário"/>
         <div className="countdown-container">
-          <Counter title="Dias" number={2}/>
-          <Counter title="Horas" number={2}/>
-          <Counter title="Minutos" number={2}/>
-          <Counter title="Segundos" number={2}/>
+          <Counter title="Dias" number={day}/>
+          <Counter title="Horas" number={hour}/>
+          <Counter title="Minutos" number={minute}/>
+          <Counter title="Segundos" number={second}/>
         </div>
       </div>
     </div>
